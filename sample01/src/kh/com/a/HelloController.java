7d7b9acfea07d7f116ef0d194e96a593f7d07bab
user2 <user2@KH_A>
2018-03-27 16:49:51 +0900

    logger 추가

--- sample01/src/kh/com/a/HelloController.java
+++ sample01/src/kh/com/a/HelloController.java
@@ -2,6 +2,8 @@ package kh.com.a;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,6 +13,8 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller		// <- Annotation 주석문, 무조건 컨트롤러 오게함!!
 public class HelloController {
 
+	private static final Logger logger = LoggerFactory.getLogger(HelloController.class);
+	
 	/*
 	@RequestMapping("/hello")
 	public ModelAndView hello() {
@@ -31,7 +35,16 @@ public class HelloController {
 		//HttpServletRequest req를 위에 붙이면 requset를 받아올수 있다...
 	//	model.addAttribute(attributeName, attributeValue) // setattribute와같다
 		
-		System.out.println("HelloController login.do");
+		logger.info("HelloController login.do");
+		
+		int amount = 1001;
+		double balance = 10000;
+		
+		logger.info("amount:{}, balance:{}", amount, balance);
+		
+		
+		
+		//System.out.println("HelloController login.do");
 		
 		return "helloSpring";	// helloSpring.jsp로 쭉간다...
 	}
